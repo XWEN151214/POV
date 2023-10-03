bd23efcd5f9233ba5633c38026d7e0f0c9099259
Hadiya <hadiya.kashif@gmail.com>
2023-10-03 19:12:25 +0500

    Updates contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,15 @@
-from django.forms import ModelForm
-from .models import Contact
+from django import forms
+from django.core.mail import send_mail
+from django.conf import settings
 
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
 
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = '__all__'
+    def send_email(self):
+        subject = 'Contact Form Submission'
+        message = f'Name: {self.cleaned_data["name"]}\nEmail: {self.cleaned_data["email"]}\nMessage: {self.cleaned_data["message"]}'
+        sender = self.cleaned_data['email']
+        recipient = [settings.DEFAULT_FROM_EMAIL]  
+        send_mail(subject, message, sender, recipient)
